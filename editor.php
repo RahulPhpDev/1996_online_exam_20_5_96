41f085e60cd00ea6193ba2e4a8fea7bbff5939b1
RahulPhpDev <44345589+RahulPhpDev@users.noreply.github.com>
2018-10-26 14:27:37 +0530

    Add files via upload
    sdfsa

---
+++ editor.php
@@ -0,0 +1,18 @@
+
+
+  <meta charset="UTF-8" />
+  <title>Maths Equations</title>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+  <script src='https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script>
+  <script>
+  MathJax.Hub.Config({
+  tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}
+});
+  </script>
+</head>
+<body>
+
+  <p style="text-align:center">I have no idea about this equation!</p>
+  <h2> <p><math xmlns="http://www.w3.org/1998/Math/MathML"><mstyle displaystyle="false"><munder><mo>&#8721;</mo><mrow><mi>sin</mi><mfenced><mo>&#8709;</mo></mfenced></mrow></munder></mstyle><mstyle displaystyle="false"><mo>&#160;</mo></mstyle><mstyle displaystyle="false"><mo>*</mo></mstyle><mstyle displaystyle="false"><mo>&#160;</mo></mstyle><mstyle displaystyle="false"><mi mathvariant="normal">&#960;</mi></mstyle><mstyle displaystyle="false"><mo>&#160;</mo></mstyle><mstyle displaystyle="false"><mo>=</mo></mstyle><mfrac bevelled="true"><mi mathvariant="normal">d</mi><mi>dx</mi></mfrac><mstyle displaystyle="false"><mo>&#160;</mo></mstyle><mstyle displaystyle="false"><mo>+</mo></mstyle><mstyle displaystyle="false"><mo>&#160;</mo></mstyle><mstyle displaystyle="false"><mroot><mn>16</mn><mn>2</mn></mroot></mstyle><mstyle displaystyle="false"><mo>&#160;</mo></mstyle><mstyle displaystyle="false"><mo>+</mo></mstyle><mstyle displaystyle="false"><mo>&#160;</mo></mstyle><mstyle displaystyle="false"><mi mathvariant="script">B</mi></mstyle><mstyle displaystyle="false"><mo>&#8734;</mo></mstyle></math></p></h2>
+</body>
+</html>
