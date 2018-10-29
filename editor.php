7ab913abe0b9f362a4e69072425a9ac25485aac0
RahulPhpDev <44345589+RahulPhpDev@users.noreply.github.com>
2018-10-29 17:19:40 +0530

    Update editor.php
    change

--- editor.php
+++ editor.php
@@ -20,3 +20,107 @@
   <p><math xmlns="http://www.w3.org/1998/Math/MathML"><mn>162</mn></math>162</p>
 </body>
 </html>
+
+ <div class = "col-md-8"> dgfdsfsda</div>
+        <style type="text/css">
+        .report{
+            background-color: #cce0ff;
+            /*border:1px solid black;*/
+            padding:30px 20px 25px 20px;
+              box-shadow: 0 0 10px rgba(0,0,0,0.6);
+    -moz-box-shadow: 0 0 10px rgba(0,0,0,0.6);
+    -webkit-box-shadow: 0 0 10px rgba(0,0,0,0.6);
+    -o-box-shadow: 0 0 10px rgba(0,0,0,0.6);
+/*none|h-offset v-offset blur spread color |inset|initial|inherit;*/
+        }
+            .report_section a {
+                    border: 1px solid #404040;
+                    padding: 8px 12px 7px 12px;
+                    line-height: 15px;
+                    border-radius: 30%;
+                    margin-top: 10px;
+                    display: block;
+                    text-align: center;
+                    font-size: 18px;
+                    color: #6666ff;
+                    font-weight: 620;
+                     /*background: #fff;*/
+                }
+
+                .answered{
+                    /*border: 2px solid #993300 !important;*/
+                    background: #00cc00;
+                    color:#ffffff !important;
+                    border:none !important;
+                 /* border-bottom:5px solid #666 !important;
+                  border-left: 2px solid transparent !important; 
+                 border-right: 5px solid transparent!important;
+                   */
+                  border-radius:30% !important;
+                }
+
+                .answered_escape{
+                       background: #ff7733;
+                    color:#ffffff !important;
+                     border:none !important;
+                }
+        </style>
+
+        <div class="col-sm-4 report">
+        
+        <div class="report_section">
+
+         <span class=""> <h2> Name: Rahull </h2> </span>
+
+         
+
+         <div class="col-md-3">
+          <a class="answered"> 1 </a>
+         </div>
+         <div class="col-md-3">
+        <a class="answered_escape"> 2 </a>
+         </div>
+         <div class="col-md-3">
+         <a class=""> 3 </a>
+         </div> <div class="col-md-3">
+         <a>4</a>
+         </div>
+         <div class="col-md-3">
+        <a> 5</a>
+         </div>
+         <div class="col-md-3">
+         <a> 6</a>
+         </div><div class="col-md-3">
+          <a> 1 </a>
+         </div>
+         <div class="col-md-3">
+        <a> 2 </a>
+         </div>
+         <div class="col-md-3">
+         <a> 3 </a>
+         </div> <div class="col-md-3">
+         <a>4</a>
+         </div>
+         <div class="col-md-3">
+        <a> 5</a>
+         </div>
+         <div class="col-md-3">
+         <a> 6</a>
+         </div><div class="col-md-3">
+          <a> 1 </a>
+         </div>
+         <div class="col-md-3">
+        <a> 2 </a>
+         </div>
+         <div class="col-md-3">
+         <a> 3 </a>
+         </div> <div class="col-md-3">
+         <a>4</a>
+         </div>
+         <div class="col-md-3">
+        <a> 5</a>
+         </div>
+         <div class="col-md-3">
+         <a> 6</a>
+         </div>
+        </div>
