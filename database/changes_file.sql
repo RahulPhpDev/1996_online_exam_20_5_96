2fb0676d6b31550c38c32f6c8f9d6656688e422a
Dev Rahul <mrrahul2016@gmail.com>
2019-03-26 17:59:51 +0530

    rc

--- database/changes_file.sql
+++ database/changes_file.sql
@@ -1,1 +1,9 @@
 ALTER TABLE `results` ADD `user_comment` TEXT NULL AFTER `edit_date`;
+
+
+git add app/Http/Controllers/Admin/AdminController.php
+git add app/Http/Controllers/Auth/UserController.php
+git add public/frontend/js/custom.min.js
+git add resources/views/admin/subscription/add-subscription.blade.php
+git add resources/views/permit/exam/attempt-exam.blade.php
+
